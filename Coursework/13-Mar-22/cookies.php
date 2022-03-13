2209b879f88086c48db2ddb2e0554d4c8cacfc37
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-03-13 14:12:57 +0200

    Formatted notes

--- Coursework/13-Mar-22/cookies.php
+++ Coursework/13-Mar-22/cookies.php
@@ -1,30 +1,30 @@
 <?php
 /*** Setting a cookie ***/
-setcookie
-(
-  $cookie_name, 
-  $cookie_value,
-  // Optional Parameters: 
-  $expire_time, // time till the cookie expires (default: till the browser closes) 
-  $cookie_file_path, // determines which directory and subdirectories where the cookie will be available (default: /)
-  $domain, // determines the domain or sub-domain where the cookie will be available (default:)
-  $is_secure, // whether or not it needs https to send (default: false)
-  $is_HTTP_only // determines whether or not non-http scripts can access the cookie (default: false)
-);
+  setcookie
+  (
+    $cookie_name, 
+    $cookie_value,
+    // Optional Parameters: 
+    $expire_time, // time till the cookie expires (default: till the browser closes) 
+    $cookie_file_path, // determines which directory and subdirectories where the cookie will be available (default: /)
+    $domain, // determines the domain or sub-domain where the cookie will be available (default:)
+    $is_secure, // whether or not it needs https to send (default: false)
+    $is_HTTP_only // determines whether or not non-http scripts can access the cookie (default: false)
+  );
 
-//Example:
-setcookie('location', 'USA', time() + 60 * 60 * 24 * 7, '/');
+  //Example:
+    setcookie('location', 'USA', time() + 60 * 60 * 24 * 7, '/');
 
 
 /*** Getting a cookie ***/
-if(isset($_COOKIE['location'])) 
-{
-  $location = $_COOKIE['location'];
-}
+  if(isset($_COOKIE['location'])) 
+  {
+    $location = $_COOKIE['location'];
+  }
 
 /*** Destroying a cookie ***/
-/*
-  1. Call setcookie with the same parameters.
-  2. Set time in the past.
-  3. There is no step 3.
-*/
+  /*
+    1. Call setcookie with the same parameters.
+    2. Set time in the past.
+    3. There is no step 3.
+  */
